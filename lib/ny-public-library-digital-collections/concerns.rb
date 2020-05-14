5e549e5c4def1f24de2951e9e522108662639506
Devone Austin <devoneaustin@Philips-MacBook-Pro.local>
2020-05-14 13:29:35 -0700

    empressofflowers

--- lib/ny-public-library-digital-collections/concerns.rb
+++ lib/ny-public-library-digital-collections/concerns.rb
@@ -4,13 +4,16 @@ module Concerns
   module Concerns::InstanceMethod
     def initialize info
       info.each {|key, value| self.send("#{key}=", value)}
-      @@all << self
     end
   end
 
   module Concerns::ClassMethod
     def self.all
       @@all
+    end
+
+    def self.save
+      @@all << self
     end
   end
 end
